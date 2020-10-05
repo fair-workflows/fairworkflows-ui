9477d267b9b1707e532daa7fa7665fa9d473c6ab
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2020-10-05 10:45:35 +0200

    Add nanopub urls

--- fairworkflows-ui/flask_app.py
+++ fairworkflows-ui/flask_app.py
@@ -33,8 +33,7 @@ def create_app():
     def update_visualization():
         workflow = cache['workflow']
         ts = time.time()
-        filepath = 'static/dag' + str(int(ts))
-        print(filepath)
+        filepath = 'static/cache/dag' + str(int(ts))
         cache['filepath'] = filepath + '.dot.png'
         workflow.draw('fairworkflows-ui/' + filepath)
 
@@ -42,12 +41,14 @@ def create_app():
     def workflow():
         if 'add_step' in request.form:
             uri = request.form['step_uri']
-            step = FairStep(uri)
+            if request.form.get('from_nanopub'):
+                step = FairStep.from_nanopub(uri)
+            else:
+                step = FairStep(uri)
             workflow = cache['workflow']
             workflow.add(step)
             update_visualization()
             return redirect('/workflow')
-        print(cache.get('filepath'))
         return render_template('workflow.html',
                                workflow=cache.get('workflow'),
                                image_path=cache.get('filepath'))
