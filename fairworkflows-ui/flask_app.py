986e3cf983d6abc2084f29b8cd397de53592a1aa
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2020-10-05 09:44:31 +0200

    Setup flask app demo

---
+++ fairworkflows-ui/flask_app.py
@@ -0,0 +1,63 @@
+import time
+
+from fairworkflows import FairWorkflow, FairStep
+from flask import Flask, render_template, request, redirect, jsonify
+
+cache = {}
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+    @app.route("/", methods=['GET', 'POST'])
+    def index():
+        all_count, in_hat_count = 2, 1
+        if request.method == 'POST':
+            if 'create_workflow' in request.form:
+                description = request.form['workflow_description']
+                cache['workflow'] = FairWorkflow(description=description)
+                update_visualization()
+                return redirect('/workflow')
+            elif 'empty_hat' in request.form:
+                return redirect('/')
+            elif 'start_game' in request.form:
+                return redirect('/game')
+            elif 'fill_hat' in request.form:
+                return redirect('/')
+        return render_template('index.html',
+                               workflow=cache.get('workflow'),
+                               all_count=all_count,
+                               in_hat_count=in_hat_count)
+
+    def update_visualization():
+        workflow = cache['workflow']
+        ts = time.time()
+        filepath = 'static/dag' + str(int(ts))
+        print(filepath)
+        cache['filepath'] = filepath + '.dot.png'
+        workflow.draw('fairworkflows-ui/' + filepath)
+
+    @app.route("/workflow", methods=['GET', 'POST'])
+    def workflow():
+        if 'add_step' in request.form:
+            uri = request.form['step_uri']
+            step = FairStep(uri)
+            workflow = cache['workflow']
+            workflow.add(step)
+            update_visualization()
+            return redirect('/workflow')
+        print(cache.get('filepath'))
+        return render_template('workflow.html',
+                               workflow=cache.get('workflow'),
+                               image_path=cache.get('filepath'))
+    return app
+
+
+def main():
+    app = create_app()
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
